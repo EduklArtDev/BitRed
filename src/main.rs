afdc2af572fb586b5aac38d83cf1049990a03f88
edukl.art <eduardo.santucci@proton.me>
2025-04-18 06:14:37 -0300

    teste de Mnemonic, e hex

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bip39::{Language, Mnemonic};
+use std::io;
+
 fn main() {
-    println!("BitRed new wallet");
+    let mut inptsenha = String::new();
+
+    println!("Digita sua senha ai: ");
+    io::stdin()
+        .read_line(&mut inptsenha)
+        .expect("Erro ao ler a linha");
+
+    //remove \n
+    let inptsenha = inptsenha.trim();
+
+    println!("Digitou: {}", inptsenha);
+
+    //.............................
+
+    let mnemonic = Mnemonic::from_entropy(&[0; 32]).unwrap();
+
+    let senha = inptsenha;
+
+    // Gera a seed com base na mnemônica + senha (BIP-39)
+    let seed = mnemonic.to_seed(senha);
+
+    println!("Seed (hex): {}", hex::encode(seed));
 }
